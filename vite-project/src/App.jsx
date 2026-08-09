17a30e260cceaae42ebc6ca8f2e9a665763163aa
krishna <hrraj3456@gmail.com>
2026-08-09 21:45:38 +0530

    Restored main website routing and added /admin path

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,44 +1,28 @@
-import './App.css'
-import { LandingPage } from './Component/Header'
-
+import './App.css';
+import { LandingPage } from './Component/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BestSeller } from './Component/BestSeller';
 import ProductPage from './Component/ProductPage';
 import CartPage from './Component/CartPage';
-import SearchPage from './Component/Searchpage'
-
+import SearchPage from './Component/Searchpage';
 import Admin from "./Component/Admin";
 
 function App() {
   return (
-    <div>
-      <Admin />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Your Main Website Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/collections/our-best-sellers" element={<BestSeller />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        
+        {/* Your Admin Dashboard Route */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App; 
-
-/* function App() {
-
-  return(
-    <BrowserRouter>
-      <Routes>
-       
-        <Route path="/" element={<LandingPage />} />
-        
-        
-        <Route path="/collections/our-best-sellers" element={<BestSeller />} />
-
-        <Route path="/products/:id" element={<ProductPage />} />
-
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
-  
-}
-
-export default App */  
- 
+export default App;
